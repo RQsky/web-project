06cd200d45ced6e1572618dbd44dee822e55528b
RQsky <36152827+RQsky@users.noreply.github.com>
2021-06-29 21:35:17 +0800

    Delete we-next-master directory

--- we-next-master/jest.config.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    roots: ['<rootDir>/test'],
-    testRegex: 'test/(.+)\\.test\\.(js?|ts?)$',
-    transform: {
-        '^.+\\.ts?$': 'ts-jest',
-    },
-    moduleFileExtensions: ['ts', 'tsx', 'js', 'jsx', 'json', 'node'],
-}
--- we-next-master/src/utils/const.ts
+++
@@ -1,6 +0,0 @@
-/**
- * @description 常亮
- * @author wangfupeng
- */
-
-export function EMPTY_FN() {}
